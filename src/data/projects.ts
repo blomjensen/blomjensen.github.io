4df6894b2f1d0eaae0fe0ea1d3cfa83b9940ab3a
blomjensen <bjorn.blom-jensen@hotmail.com>
2025-12-28 02:14:24 +0100

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -30,8 +30,6 @@ export const projects: Project[] = [
       { src: "/projects/rockfall_gallery/01_plan.png", caption: "Plan 1:500" },
       { src: "/projects/rockfall_gallery/02_section.png", caption: "Snitt 1:50" },
       { src: "/projects/rockfall_gallery/03_model.png", caption: "Modell 1:200" },
-],
-      // ...
     ],
     processImages: [
       // { src: "/projects/rockfall_gallery/10_process.png", caption: "Prosess" },
